24146d96e477823e18f53661408356fbdb0e0a6e
Khunaiko <pasuaiko21089@gmail.com>
2023-10-24 15:42:45 +0700

    first commit

---
+++ compare.js
@@ -0,0 +1,16 @@
+//compaer operator
+//1.Equal OPerator
+console.log(2 == '2');//true
+console.log(2 == '2');//true
+
+//2.Not Equal Operator
+console.log(2 != 2);//false
+console.log('hello' != 'Hello');//true
+
+//3.Stric equal OPerator
+console.log(2 === 2);//true
+console.log(2 === '2');//false 
+
+//4.Stric not equal OPerator
+console.log(2 !== '2');//true
+console.log(2 !==  2);//false
